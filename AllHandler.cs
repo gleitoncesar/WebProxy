d622be9489dc7c52c8663a71f4bf37eabf1fa3d5
gleitoncesar <37295837+gleitoncesar@users.noreply.github.com>
2024-10-03 17:25:30 -0300

    Update AllHandler.cs

--- AllHandler.cs
+++ AllHandler.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.IO.Compression;
 using System.Net;
+using System.Text;
 using System.Web;
-
 
 namespace WebProxy
 {
@@ -26,31 +27,44 @@ namespace WebProxy
             var res = newReq.GetResponse() as HttpWebResponse;
 
             context.Response.ContentType = res.ContentType;
-            foreach(Cookie c in res.Cookies)
+            foreach (Cookie c in res.Cookies)
                 context.Response.Cookies.Add(new HttpCookie(c.Name, c.Value) { Expires = c.Expires, Secure = c.Secure, Path = c.Path });
 
             foreach (var headerKey in res.Headers.AllKeys)
                 context.Response.Headers.Add(headerKey, res.Headers[headerKey]);
 
-			byte[] buff = new byte[0x1000];
+            byte[] buff = new byte[0x1000];
 
             var ms = new MemoryStream();
 
-            ms.Write(new byte[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 0, 10);
-			
-			int cnt = 0;
+            //ms.Write(new byte[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 0, 10);
+
+            int cnt = 0;
             using (var resStream = res.GetResponseStream())
             {
                 while ((cnt = resStream.Read(buff, 0, buff.Length)) > 0)
                     ms.Write(buff, 0, cnt);
             }
 
-			ms.Position = 0;
+            var ms2 = new MemoryStream(Encoding.UTF8.GetBytes(Convert.ToBase64String(ms.ToArray())));
 
+            //ms.Position = 0;
             cnt = 0;
-            while ((cnt = ms.Read(buff, 0, buff.Length)) > 0)
+
+            while ((cnt = ms2.Read(buff, 0, buff.Length)) > 0)
                 context.Response.OutputStream.Write(buff, 0, cnt);
-            
+
+        }
+
+        private static byte[] Compress(Stream input)
+        {
+            using (var compressStream = new MemoryStream())
+            using (var compressor = new DeflateStream(compressStream, CompressionMode.Compress))
+            {
+                input.CopyTo(compressor);
+                compressor.Close();
+                return compressStream.ToArray();
+            }
         }
 
         private HttpWebRequest CreateRequest(HttpRequest source, string targetSite)
@@ -72,10 +86,10 @@ namespace WebProxy
 
             //request.KeepAlive = source.Headers["KeepAlive"];
             request.Method = source.HttpMethod;
-            if(source.UrlReferrer!=null)
+            if (source.UrlReferrer != null)
                 request.Referer = source.UrlReferrer.ToString();
             request.UserAgent = source.UserAgent;
-            
+
             return request;
         }
     }
